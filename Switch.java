f4d41c7f9c4874548581d09258322b4661245c93
urzprashanth <44563835+urzprashanth@users.noreply.github.com>
2018-12-17 11:58:57 +0530

    switch related

---
+++ Switch.java
@@ -0,0 +1,20 @@
+public class Switch {
+    public static void main(String[] args) {
+        //String name = "1";
+        int i = 1;
+
+        switch (i){
+            case 1:
+                System.out.println("one");
+                //break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("three");
+                break;
+
+
+        }
+    }
+}
